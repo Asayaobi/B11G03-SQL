1b77487c8248f4eb98126ba512e2a7d8dac6e4b7
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-01-31 18:04:57 +0700

    Modify bookings table

--- bookings_table.sql
+++ bookings_table.sql
@@ -2,7 +2,7 @@ CREATE TABLE bookings (
 	booking_id SERIAL PRIMARY KEY,
 	guest_id INT REFERENCES users(user_id) NOT NULL,
 	house_id INT REFERENCES houses(house_id) NOT NULL,
-	check-in_date DATE NOT NULL,
-	check-out_date DATE NOT NULL,
+	check_in_date DATE NOT NULL,
+	check_out_date DATE NOT NULL,
 	total_price FLOAT NOT NULL
 )
