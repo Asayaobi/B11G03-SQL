5610a3adcefa355aeac03de8126f1cd644a62b0f
ZulejkaRibizli <146037927+ZulejkaRibizli@users.noreply.github.com>
2024-01-31 18:03:36 +0700

    Query to create user tables

---
+++ users_table.sql
@@ -0,0 +1,10 @@
+--Query to create users table
+CREATE TABLE users(
+	user_id serial PRIMARY KEY,
+	first_name VARCHAR(50) NOT NULL,
+	last_name VARCHAR(50),
+	profile_pic VARCHAR(255),
+	email VARCHAR(50) NOT NULL,
+	password VARCHAR(50) NOT NULL
+);
+
