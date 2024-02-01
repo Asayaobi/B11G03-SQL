9c48205e9615e433244dc2b25608f950a7c37f25
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-02-01 16:54:37 +0700

    Added query to read the reviews

--- reviews_queries.sql
+++ reviews_queries.sql
@@ -12,3 +12,33 @@ VALUES
 	'2024-02-01 10:00:00',
 	5,
 	'Great place to stay')
+
+--Query to read reviews
+--Total reviews
+SELECT 
+	COUNT(review_id)
+FROM 
+	reviews
+WHERE 
+	house_id = 1
+
+--Average rating
+SELECT
+	AVG(reviews.rating)
+FROM
+	reviews
+
+--Read the reviews
+SELECT
+	users.profile_pic,
+	reviews.review_date,
+	users.first_name,
+	users.last_name,
+	reviews.rating,
+	reviews.review_text
+FROM
+	users
+LEFT JOIN 
+	reviews ON reviews.reviewer_id = users.user_id
+WHERE
+	reviews.house_id = 1
