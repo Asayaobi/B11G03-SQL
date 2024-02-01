e8c49b1641dbe84967ed6e08b769bcac094706b3
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-02-01 15:54:00 +0700

    Add reviewer_id column to reviews table

--- reviews_table.sql
+++ reviews_table.sql
@@ -2,6 +2,7 @@
 CREATE TABLE reviews (
 	review_id SERIAL PRIMARY KEY,
 	house_id INT REFERENCES houses(house_id) NOT NULL,
+	reviewer_id INT REFERENCES users(user_id) NOT NULL,
 	review_date TIMESTAMP NOT NULL,
 	rating INT NOT NULL,
 	review_text TEXT
