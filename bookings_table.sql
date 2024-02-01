b23642a0799e8ffbbe1d68ab74d74034df8cb92d
ZulejkaRibizli <146037927+ZulejkaRibizli@users.noreply.github.com>
2024-02-01 13:34:26 +0700

    Update bookings_table.sql

--- bookings_table.sql
+++ bookings_table.sql
@@ -6,3 +6,20 @@ CREATE TABLE bookings (
 	check_out_date DATE NOT NULL,
 	total_price INT NOT NULL
 )
+
+INSERT INTO bookings (
+	booking_id,
+	guest_id,
+	house_id,
+	check_in_date,
+	check_out_date,
+	total_price
+)
+VALUES (
+	'1',
+	'1',
+	'1',
+	'2001.01.12',
+	'2001.01.22',
+	'345'
+)
