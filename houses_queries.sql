7dfe1dfbc5bbde32f8655bda59bbdf218bcd4644
Jesus <40635808+jgarciagalvez@users.noreply.github.com>
2024-02-01 13:38:55 +0700

    Create House in DB with related pics

---
+++ houses_queries.sql
@@ -0,0 +1,26 @@
+-- Queries to create a house
+-- Next query creates a house in house table
+
+INSERT INTO houses (
+	location,
+	bedrooms,
+	bathrooms,
+	price_per_night,
+	description,
+	host_id)
+VALUES (
+	'Koh Phangan, Thailand',
+	3,
+	1,
+	19.99,
+	'lorem ipsum lorem ipsum',
+	1);
+
+-- Next query creates the related pics in house_pics table
+
+INSERT INTO house_pics (
+	house_id, 
+	url)
+VALUES (
+	1,
+	'photo1.png');
