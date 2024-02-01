f961bdb9a61b304b92c51b3023b800ffc9f98c49
ZulejkaRibizli <146037927+ZulejkaRibizli@users.noreply.github.com>
2024-02-01 18:05:20 +0700

    fixing update houses

--- update_houses.sql
+++ update_houses.sql
@@ -1,23 +1,8 @@
-UPDATE houses
-	SET location = 'Bali'
-	WHERE location = 'Koh Phangan';
-	
-UPDATE houses
-	SET bedrooms = 4
-	WHERE bedrooms = 1;
-	
-UPDATE houses
-	SET bathrooms = 1
-	WHERE bathrooms = 2;
-	
-UPDATE houses
-	SET price_per_night = price_per_night *1.1
-	WHERE price_per_night > 100;
-	
-UPDATE houses
-	SET description = 'NICE'
-	WHERE description = 'not nice;
-	
-UPDATE houses
-	SET picture = 'https://randomuser.me/api/portraits/men/82.jpg'
-	WHERE picture = 'https://randomuser.me/api/portraits/women/0.jpg'
+UPDATE houses 
+
+SET location = 'Bali',
+	bedrooms = 4,
+	bathrooms = 1,
+ 	description = 'NICE'
+	WHERE house_id = 1;
+
