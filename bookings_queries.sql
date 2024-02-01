c596a4bd5ee3c6a0fb0195f37d3a63ea54f5fda7
ZulejkaRibizli <146037927+ZulejkaRibizli@users.noreply.github.com>
2024-02-01 13:47:12 +0700

    Create bookings_queries.sql

---
+++ bookings_queries.sql
@@ -0,0 +1,16 @@
+INSERT INTO bookings (
+	booking_id,
+	guest_id,
+	house_id,
+	check_in_date,
+	check_out_date,
+	total_price
+)
+VALUES (
+	'1',
+	'1',
+	'1',
+	'2001.01.12',
+	'2001.01.22',
+	'345'
+)
